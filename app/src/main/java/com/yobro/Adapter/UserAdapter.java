1a5fde5f5920f596416f8cca639e0d439e899921
Pawan Dhungana <pwndhungana3@gmail.com>
2018-10-06 23:50:00 -0500

    chat

---
+++ app/src/main/java/com/yobro/Adapter/UserAdapter.java
@@ -0,0 +1,78 @@
+package com.yobro.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.yobro.MessageActivity;
+import com.yobro.Model.User;
+import com.yobro.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter {
+
+    private Context mContext;
+    private List<User> mUsers;
+    public TextView username;
+    public ImageView profile_image;
+
+    public UserAdapter(Context mContext, List<User> mUsers){
+        this.mUsers = mUsers;
+        this.mContext=mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final User user = mUsers.get(position);
+        username.setText(user.getUsername());
+        if(user.getImageURL().equals("default")){
+            profile_image.setImageResource(R.mipmap.ic_launcher);
+        }
+        else
+        {
+            Picasso.get().load(Uri.parse(user.getImageURL())).into(profile_image);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid",user.getId());
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+        }
+    }
+}
--- app/src/main/java/com/yobro/MapActivityHome.java
+++ app/src/main/java/com/yobro/MapActivityHome.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -47,8 +49,6 @@ public class MapActivityHome extends AppCompatActivity
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private GoogleSignInClient mGoogleSignInClient;
     FirebaseHelper firebaseHelper = new FirebaseHelper();
-
-    private Toolbar mToolbar;
 
     //Button
     Switch onlineBtn;
@@ -111,7 +111,8 @@ public class MapActivityHome extends AppCompatActivity
         user_Email = headerView.findViewById(R.id.userEmail);
         userProfileView = headerView.findViewById(R.id.userProfilePic);
         getUserProfile();
-    }
+
+        }
 
     private void getUserProfile() {
 
@@ -158,6 +159,8 @@ public class MapActivityHome extends AppCompatActivity
         }
     }
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -192,8 +195,14 @@ public class MapActivityHome extends AppCompatActivity
             //startActivity(new Intent(this, GoogleMaps.class));
             fragment = new MapFragment();
 
-
-        } else if (id == R.id.nav_setting) {
+        }
+        else if (id == R.id.nav_inbox) {
+            fragment = new Chat();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction Replace = fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment );
+            Replace.addToBackStack(null).commit();
+        }
+        else if (id == R.id.nav_setting) {
 
 
         } else if (id == R.id.nav_history) {
@@ -253,4 +262,8 @@ public class MapActivityHome extends AppCompatActivity
     }
 
 
+    private class ViewPagerAdapter {
+        public ViewPagerAdapter(FragmentManager supportFragmentManager) {
+        }
+    }
 }
